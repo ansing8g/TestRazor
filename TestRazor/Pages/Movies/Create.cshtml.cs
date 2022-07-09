af2184047c719d4452386e04918536cc1570d7aa
ansing8g <ansing8@gmail.com>
2022-07-10 03:30:31 +0900

    튜토리얼

---
+++ TestRazor/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TestRazor.Models;
+
+namespace TestRazor.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly TestRazor.Data.RazorPagesMovieContext _context;
+
+        public CreateModel(TestRazor.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Movie == null || Movie == null)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
